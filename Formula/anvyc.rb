bf6e8108e9a6a058410b856596bd408ad1c4b9de
16bitdo <166486891+16bitdo@users.noreply.github.com>
2026-09-02 14:00:05 +0900

    chore: anvyc 0.22.0 (#7)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/anvyc.rb
+++ Formula/anvyc.rb
@@ -9,8 +9,8 @@ class Anvyc < Formula
 
   desc "여러 장치에서 개발 도구 설정을 안전하게 백업/비교/복원/동기화하는 macOS CLI"
   homepage "https://github.com/16bitdo/anvyc"
-  url "https://github.com/16bitdo/anvyc/releases/download/v0.21.0/anvyc-0.21.0.tar.gz"
-  sha256 "0c7b752dc14c79576f3002b67be7b2a836c447012e651084ab2edcd0c962db4f"
+  url "https://github.com/16bitdo/anvyc/releases/download/v0.22.0/anvyc-0.22.0.tar.gz"
+  sha256 "04448df6ea1218ab2e7f14002686371cca1a26bab94264f7d8341d5a5d95f4b1"
   license "MIT"
 
   depends_on "python@3.13"
